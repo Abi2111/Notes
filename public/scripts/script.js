2b976d369b6f4caf8de6a246aff4fe054733e501
avinashabhi870@gmail.com <avinashabhi870@gmail.com>
2023-12-19 10:08:19 +0530

    Added loading animation

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,12 @@
 const menu_btn = document.querySelector('.menu_bar');
 const navlinks = document.querySelector('.nav_main');
 const hero = document.querySelector('.hero');
+const loader = document.querySelector('.loader_container');
+
+window.addEventListener('load', () => {
+  loader.classList.add('loaded');
+});
+
 menu_btn.addEventListener('click', () => {
   navlinks.classList.toggle('active');
 });

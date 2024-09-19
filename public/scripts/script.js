3559e53025f9517c2fa87a6ca0ec7d0768503b2b
avinash <avinashabhi870@gmail.com>
2024-09-19 11:32:12 +0530

    final commit

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,5 @@
 const menu_btn = document.querySelector('.menu_bar');
-const navlinks = document.querySelector('.nav_main');
+const navbar = document.querySelector('.navbar');
 const hero = document.querySelector('.hero');
 const loader = document.querySelector('.loader_container');
 
@@ -10,8 +10,8 @@ window.addEventListener('load', () => {
 });
 
 menu_btn.addEventListener('click', () => {
-  navlinks.classList.toggle('active');
+  navbar.classList.toggle('active');
 });
 hero.addEventListener('click', () => {
-  navlinks.classList.toggle('active');
+  navbar.classList.toggle('active');
 });
